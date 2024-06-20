323a59a1c48767e0a52f9dde6451c8d5ae75c17a
Jiho Sohn <jiho@HummingBird.attlocal.net>
2024-06-19 23:00:21 -0400

    UPDATED headers

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,18 +7,18 @@ export default function Grid () {
       <Container className="grid-container">
         <Row className="g-5">
           <Col sm={12} md={4}>
-            <h2 className="text-center">Why BocaCode?</h2>
-            <p>Being a software engineer allows individuals to harness the power of technology and use it to create innovative solutions to complex problems in order to develop new ways to make people's lives easier and more efficient.</p>
+            <h2 className="text-center">Tech Innovator?</h2>
+            <p className="text-center">Being a software engineer allows individuals to harness the power of technology and use it to create innovative solutions to complex problems in order to develop new ways to make people's lives easier and more efficient.</p>
           </Col>
 
           <Col sm={12} md={4}>
-            <h2 className="text-center">Exciting Project?</h2>
-            <p>Pursuit of knowledge and understanding was essential to human happiness and flourishing. I value the practical application of scientific knowledge in improving human life.</p>
+            <h2 className="text-center">Knowledge Quest?</h2>
+            <p className="text-center">Pursuit of knowledge and understanding was essential to human happiness and flourishing. I value the practical application of scientific knowledge in improving human life.</p>
           </Col>
 
           <Col>
-            <h2 className="text-center">Ideal Work Place?</h2>
-            <p>An environment where intellectual inquiry, encourages ongoing learning and growth, and fosters a culture of creativity and collaboration.</p>
+            <h2 className="text-center">Creative Collaboration?</h2>
+            <p className="text-center">An environment where intellectual inquiry, encourages ongoing learning and growth, and fosters a culture of creativity and collaboration.</p>
           </Col>
         </Row>
       </Container>
